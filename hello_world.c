90144b92c66140a0fd5431e92ab14dc1d7fb3580
Developer-QuEST <100339062+Developer-QuEST@users.noreply.github.com>
2022-02-24 12:58:30 +0530

    Simple C program

---
+++ hello_world.c
@@ -0,0 +1,21 @@
+/****************************************************************************
+ * Copyright (C) 2022 by QuEST Global Engineering Services Pvt LTD          *
+ ****************************************************************************/
+// header files
+#include <stdio.h>
+
+// constants
+
+// global variables
+
+// function declarations
+
+// functions
+int main()
+{
+
+	// print hello world to the terminal
+	printf("Hello World");
+
+	return 0;
+}
